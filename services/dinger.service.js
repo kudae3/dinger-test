b1d9e49aad750bd3d8dd455b4c7921f70c16adf1
Kudae Sithu <kudaesithu2@gmail.com>
2026-03-20 16:07:31 +0630

    refactor dinger service: remove unnecessary blank line

--- services/dinger.service.js
+++ services/dinger.service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = process.env.DINGER_BASE_URL;
-
 const projectName = process.env.DINGER_PROJECT_NAME;
 const apiKey = process.env.DINGER_API_KEY;
 const merchantName = process.env.DINGER_MERCHANT_NAME || "PayT";
